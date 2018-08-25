d5626a8a0acb8b3c259f49201f3a8a1194b1119c
brianmj <spamthiseml@hotmail.com>
2018-08-25 01:44:30 -0400

    Initial Commit

---
+++ base_game_mac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  base_game_mac
+//
+//  Created by Brian Jones on 8/25/18.
+//  Copyright © 2018 Brian Jones. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
